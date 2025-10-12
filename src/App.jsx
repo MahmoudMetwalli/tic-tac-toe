e5d419cab101a3fe5eee592921fea3d3c9d100c1
Mahmoud Metwalli <mahmoud.metwaly@vconnct.com>
2025-10-12 11:40:47 +0300

    Game Board Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Player from "./components/player.jsx";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   return (
-    <div id="game-container">
-      <ol>
-        <Player initialName="Player 1" symbol="X" />
-        <Player initialName="Player 2" symbol="O" />
-      </ol>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
   );
 }
 
